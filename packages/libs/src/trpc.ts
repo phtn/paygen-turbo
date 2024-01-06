33d80e7e544da6fec1577d53d66f22f47ef15304
Jun Orville Lecena <jun.lecena@comptrolla.com>
2024-01-07 03:41:24 +0800

    @server

--- packages/libs/src/trpc.ts
+++ packages/libs/src/trpc.ts
@@ -1,5 +1,5 @@
-import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 import { MergedRouter } from "@server/src/routers";
+import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 
 const trpc = createTRPCProxyClient<MergedRouter>({
   links: [httpBatchLink({ url: "http://localhost:8080/trpc" })],
